566bc60a99937a6fa69886ee229082bb082d5671
Cloud Dark <syahdan.if16@student.unusa.ac.id>
2021-04-23 08:54:01 +0700

    Add files via upload

---
+++ key.php
@@ -0,0 +1,38 @@
+
+<?php
+
+	$data= '
+aku pengen kamu, iya kamuuuuuuu
+  ';
+
+  $result = "ubah string ini sesuai kebutuhan kamu"
+	$key = md5($result); //ubah key menjadi md5
+  echo "key: $key";
+	?>
+<br>
+	<?php
+	$method="AES-128-CTR";
+	$key =  $result;
+	$iv="RLv~K1a37)OOb49@9q[5cI9P/mGfmm9c|5r_20q(b[sP9l*Hl88FfCWN~_2r]o2q";
+	$option=0;
+
+	echo "Data sebelum dienkripsi = $data <br/>";
+
+	$dataTerenkripsi=openssl_encrypt($data, $method, $key, $option, $iv);
+	echo "Data setelah dienkripsi = $dataTerenkripsi<br/>";
+
+	$dataDecrypt=openssl_decrypt($dataTerenkripsi, $method, $key, $option, $iv);
+	echo "Data terenkripsi ketika di decrypt = $dataDecrypt";
+
+?>
+<br>
+
+<?php
+//aktifkan jika data yang dikelola berupa json
+// $json = $dataDecrypt;
+//
+// $obj = json_decode($json);
+// print $obj->{'name'};
+// print $obj->{'age'};
+// print $obj->{'city'};
+ ?>
